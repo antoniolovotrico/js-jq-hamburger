520feae83ff98fd877cb00a4d7dc130f9258f69e
Antonio <antonio.lovotrico@icloud.com>
2020-11-26 15:40:47 +0100

    quest completed

---
+++ main.js
@@ -0,0 +1,9 @@
+/////Hamburger menu:mostrare / nascondere il menu principale
+
+$(".fa-bars").click(function(){
+    $(".hamburger-menu").show();
+})
+
+$(".fa-times").click(function(){
+    $(".hamburger-menu").hide();
+})
